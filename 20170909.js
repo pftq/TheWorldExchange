d7d448de31e74382c613ef99ba521bdf1b5c428a
pftq <pftq@pftq.com>
2017-09-09 20:39:18 -0700

    Add files via upload
    Removed unused JS libraries and replaced all min versions with non-min versions.

--- 20170909.js
+++ 20170909.js
@@ -2845,10 +2845,7 @@ function getAllDisplayNames(exitFunction, startIndex) {
       }
       if(transactions.length>=options.limit)
         getAllDisplayNames(exitFunction, startIndex); // get next batch if this one was full
-      else {
-        updateSymbols();
-        exitFunction();
-      }
+      else exitFunction();
     }
   }, function(err) {
     console.log("Display name retrieval error: "+err);
